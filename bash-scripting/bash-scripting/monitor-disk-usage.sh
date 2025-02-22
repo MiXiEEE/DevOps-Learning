02b8129dce9dbcb61027cf475f5d0c89176f7099
MiXiEEE <wolfzyygm@inbox.lv>
2025-02-22 15:20:13 +0200

    added log file where to save warnings if disk space is low

--- bash-scripting/bash-scripting/monitor-disk-usage.sh
+++ bash-scripting/bash-scripting/monitor-disk-usage.sh
@@ -6,6 +6,7 @@ RESET="\e[0m"
 
 # Get disk usage
 disk_info=$(df -Ph /mnt/c 2>/dev/null)
+LOG_DIR="/home/mika/log-dir" # Path to log folder
 if [ $? -ne 0 ]; then
 	echo -e "${RED}Could not retrieve disk info for /mnt/c${RESET}"
 	exit
@@ -23,5 +24,6 @@ echo -e "${CYAN}Available Space: $available_space%${RESET}"
 
 # Check if available space is below 5%
 if [ "$available_space" -le 10 ]; then
-	echo -e "${RED}Your available space is less than 10%${RESET}"
+	echo -e "${RED}Your available space in C: is less than 10%${RESET}"
+	echo "Warning: low amount of space in C: $available_space% Date: $(date -I)" >> "$LOG_DIR/disk-space.log"
 fi
