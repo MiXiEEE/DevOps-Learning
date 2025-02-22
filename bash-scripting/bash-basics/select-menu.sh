98ee37d3efb11b1218f505c37ed5ce1bc67746d1
MiXiEEE <wolfzyygm@inbox.lv>
2025-02-22 18:49:58 +0200

    switch case example

---
+++ bash-scripting/bash-basics/select-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "Enter your number from 1-5: " number
+
+if [[ "$number" =~ ^[1-5]$ ]]; then
+	case $number in
+		"1")
+			echo "you picked option 1"
+			;;
+		"2")
+			echo "you picked option 2"
+			;;
+		"3")
+			echo "you picked option 3"
+			;;
+		"4")
+			echo "you picked option 4"
+			;;
+		"5")
+			echo "you picked option 5"
+			;;
+	esac
+else
+	echo "invalid input!"
+fi
