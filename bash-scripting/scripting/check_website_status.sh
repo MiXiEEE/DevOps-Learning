dc02ef3c8b7dc300a7e57f36e75305fbe57c1061
MiXiEEE <wolfzyygm@inbox.lv>
2025-02-23 18:55:12 +0200

    updated check website status script with nc 80,443 port checks

--- bash-scripting/scripting/check_website_status.sh
+++ bash-scripting/scripting/check_website_status.sh
@@ -8,6 +8,7 @@ check_website_status() {
 
 	response=$(curl -Is "$full_webname")
 
+	# gets curls status code e.g 200
 	status_code=$(echo "$response" | awk '/^HTTP/{print $2}')
 
 	# if curl fails, logs error no response
@@ -16,6 +17,12 @@ check_website_status() {
 	fi
 
 	timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+	echo "----------------------------------------"
+	# verifies if port 80 (HTTP) or 443 (HTTPS) is open
+	for port in 80 443; do
+		port_status=$(nc -zv "$website_name" "$port" 2>&1)
+		echo "[$timestamp] Port $port: $port_status" | tee -a "$LOGS"
+	done
 
 	# Log the output
 	echo "[$timestamp] Checking: $full_webname" | tee -a "$LOGS"
@@ -30,6 +37,7 @@ if [ -z "$website_name" ]; then
 	echo "input is empty"
 	exit 1
 fi
+
 
 # trap is a built-in command that allows you to specify commands that should be executed like ctrl + c to exit
 trap 'echo "Monitoring stopped."; exit 0' INT
